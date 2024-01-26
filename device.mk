3177f3bddf325ab7661127bdfda3e6af2ad5d1b9
Jiyong Park <jiyong@google.com>
2024-01-26 14:24:59 +0100

    m33x: Declare support for updatable APEXes
    Change-Id: I61ca808d942d9eae14b30cd1754820c599ea7ff3

--- device.mk
+++ device.mk
@@ -20,6 +20,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 DEVICE_PATH := device/samsung/m33x
 
